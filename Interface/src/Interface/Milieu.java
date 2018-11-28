ff60896c2171e44d8b6edccde661c9d329069e8e
AntoineLeCodeur <45263226+AntoineLeCodeur@users.noreply.github.com>
2018-11-28 23:54:05 +0100

    Milieu champ

--- Interface/src/Interface/Milieu.java
+++ Interface/src/Interface/Milieu.java
@@ -3,13 +3,22 @@ package Interface;
 public class Milieu {
 	private Forme forme;
 	
-	public Milieu(){
+	public Milieu(Forme forme){
+		this.forme=forme;
 		
 	}
 
 	@Override
 	public String toString() {
 		return "Milieu []";
+	}
+
+	public Forme getForme() {
+		return forme;
+	}
+
+	public void setForme(Forme forme) {
+		this.forme = forme;
 	}
 
 }
