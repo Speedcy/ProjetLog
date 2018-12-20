54bf7040555d836b33bc5d1d509a6d93332bd0de
Speedcy <cyprien.castello@supelec.fr>
2018-12-20 13:01:41 +0100

    Presentation

--- Interface/src/Interface/Main.java
+++ Interface/src/Interface/Main.java
@@ -20,24 +20,38 @@ public class Main {
 
 		// SIMULATION 1 : Particules Charg�es
 		
+		int t_step = 50;
+		int x_fenetre = 500;
+		int y_fenetre = 500;
+		
+		int nbParticules = 2;
 		double masse = 9.1*Math.pow(10, -31);
 		double charge = -1.6*Math.pow(10,-19);
 		
-		Config config = new Config(50, 1000, 500);
+		Config config = new Config(t_step, x_fenetre, y_fenetre);
 	
-		config.ajoutParticules(2, charge, masse); // ajout de particules � vitesse initiale nulle et de signes al�toires
+		config.ajoutParticules(nbParticules, charge, masse); // ajout de particules � vitesse initiale nulle et de signes al�toires
 		
-		config.ajoutChampElec(200., 0., 1., 0., -0.000000000005, 0.);
+		//config.ajoutChampElec(200., 0., 1., 0., -0.000000000005, 0.);
 		
 		
 		// SIMULATION 2 : Plague
 
-		/*double freqMort=10;
+		/*int t_step = 50;
+		int x_fenetre = 500;
+		int y_fenetre = 500;
+		
+		double freqMort=10;
 		double peur=0.05;
-		Config2 config = new Config2(40, 500, 500);
-		config.ajouteDocteurs(3);
-		config.ajoutePersonne(60,freqMort,peur); // ajout de particules � vitesse initiale nulle et de signes al�toires
-		config.ajouteConfinement(50);*/
+		int nbDocteurs = 3;
+		int nbPersonnes = 50;
+		int tailleConfinement = 50;
+		
+		
+		Config2 config = new Config2(t_step, x_fenetre, y_fenetre);
+		config.ajouteDocteurs(nbDocteurs);
+		config.ajoutePersonne(nbPersonnes,freqMort,peur); // ajout de particules � vitesse initiale nulle et de signes al�toires
+		config.ajouteConfinement(tailleConfinement);*/
 
 		
 		
